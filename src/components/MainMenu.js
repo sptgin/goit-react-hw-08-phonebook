402d7f52a036f65ef16f6320f6824ddb9fd34edc
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-12-02 23:59:39 +0200

    fix styles

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -42,7 +42,6 @@ export default function MainMenu() {
                   Login
                 </Link>
               </li>
-
               <li className="mainmenu__loginlist-item">
                 <Link className="mainmenu__link" to="/register">
                   Register
